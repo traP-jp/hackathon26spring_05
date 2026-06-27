da26d0a2937ea0e229b8ae98e47686aa45f4ee1b
YuHima03 <67862122+YuHima03@users.noreply.github.com>
2026-06-27 16:28:13 +0900

    feat: いらないリポジトリ消す

--- Qpid/domain/login_user_retriever.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type LoginUserRetriever interface {
-	// ユーザーがログイン中かどうかを判定する
-	IsUserLoggedIn() bool
-	// ログイン中のユーザー名を取得する
-	GetLoginUser() (string, error)
-}
--- Qpid/repository/suggestion_repository.go
+++ Qpid/repository/suggestion_repository.go
@@ -6,6 +6,4 @@ import "github.com/traP-jp/hackathon26spring_05/Qpid/domain"
 type SuggestionRepository interface {
 	// おすすめユーザーを取得する
 	ListSuggestions(username string, limit int) ([]domain.Suggestion, error)
-	// 類似度を保存する
-	UpsertSimilarity(usernameA, usernameB string, similarity float64) error
 }
