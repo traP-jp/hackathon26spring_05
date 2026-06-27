87c354be0caf15a375d5c6ad176f302d7f660ac7
eoate <tsubasa20060117@icloud.com>
2026-06-27 21:30:04 +0900

    NopeUserを実装

--- Qpid/infrastructure/action_repository.go
+++ Qpid/infrastructure/action_repository.go
@@ -2,6 +2,11 @@ package infrastructure
 
 import "github.com/traP-jp/hackathon26spring_05/Qpid/domain"
 
+const (
+	actionStatusNope = 0
+	actionStatusLike = 1
+)
+
 // ユーザーを LIKE する。
 func (r *repositoryImpl) LikeUser(fromUsername, toUsername string) error {
 	return nil
@@ -9,7 +14,11 @@ func (r *repositoryImpl) LikeUser(fromUsername, toUsername string) error {
 
 // ユーザーを NOPE する。
 func (r *repositoryImpl) NopeUser(fromUsername, toUsername string) error {
-	return nil
+	_, err := r.db.Exec(
+		`INSERT INTO actions (id, from_username, to_username, status) VALUES (UUID(), ?, ?, ?)`,
+		fromUsername, toUsername, actionStatusNope,
+	)
+	return err
 }
 
 // LIKE したユーザーを取得する。
