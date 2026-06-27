df85d7dd7838c7c5ab168b534426bfa730e56793
genMira <{weilianzhuantongyin@gmail.com}>
2026-06-27 17:42:49 +0900

    usernameをprimary keyに

--- Qpid/db/init/0_Schema.sql
+++ Qpid/db/init/0_Schema.sql
@@ -1,5 +1,4 @@
 CREATE TABLE users (
-  id VARCHAR(36) NOT NULL,
   username VARCHAR(255) NOT NULL,
   created_at DATETIME NOT NULL,
   major VARCHAR(255) NOT NULL,
@@ -9,34 +8,34 @@ CREATE TABLE users (
   dislike_topic VARCHAR(255),
   dislike_value VARCHAR(255),
   bio TEXT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (username)
 );
 
 CREATE TABLE tags (
-  user_id VARCHAR(36) NOT NULL,
+  username VARCHAR(36) NOT NULL,
   name VARCHAR(255) NOT NULL,
   affinity TINYINT NOT NULL,
   strength FLOAT NOT NULL,
-  PRIMARY KEY (user_id, name)
+  PRIMARY KEY (username, name)
 );
 
 CREATE TABLE affiliations (
-  user_id VARCHAR(36) NOT NULL,
+  username VARCHAR(36) NOT NULL,
   team VARCHAR(255) NOT NULL,
-  PRIMARY KEY (user_id, team)
+  PRIMARY KEY (username, team)
 );
 
 CREATE TABLE actions (
   id VARCHAR(36) NOT NULL,
-  from_userid VARCHAR(36) NOT NULL,
-  to_userid VARCHAR(36) NOT NULL,
+  from_username VARCHAR(36) NOT NULL,
+  to_username VARCHAR(36) NOT NULL,
   status TINYINT NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE icons(
-  id VARCHAR(36) NOT NULL,
+  username VARCHAR(36) NOT NULL,
   icon LONGBLOB NOT NULL
   mime_type VARCHAR(50) NOT NULL
-  PRIMARY KEY (user_id)
+  PRIMARY KEY (username)
 );
