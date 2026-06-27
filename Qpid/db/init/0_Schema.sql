876d3cb48fbd57c60302f83fbe2f8983dd3ffcc9
genMira <{weilianzhuantongyin@gmail.com}>
2026-06-27 18:10:05 +0900

    レビューを受けての変更

--- Qpid/db/init/0_Schema.sql
+++ Qpid/db/init/0_Schema.sql
@@ -1,41 +1,45 @@
 CREATE TABLE users (
   username VARCHAR(255) NOT NULL,
-  created_at DATETIME NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   major VARCHAR(255) NOT NULL,
   hometown VARCHAR(255),
   like_topic VARCHAR(255),
   like_value VARCHAR(255),
   dislike_topic VARCHAR(255),
   dislike_value VARCHAR(255),
+  tool VARCHAR(255),
+  usual_situation VARCHAR(255),
   bio TEXT,
   PRIMARY KEY (username)
 );
 
+CREATE INDEX idx_like_topic ON users (like_topic);
+CREATE INDEX idx_like_value ON users (like_value);
+CREATE INDEX idx_dislike_topic ON users (dislike_topic);
+CREATE INDEX idx_dislike_value ON users (dislike_value);
+
 CREATE TABLE tags (
   username VARCHAR(36) NOT NULL,
   name VARCHAR(255) NOT NULL,
-  affinity TINYINT NOT NULL,
-  strength FLOAT NOT NULL,
   PRIMARY KEY (username, name)
 );
 
-CREATE TABLE affiliations (
-  username VARCHAR(36) NOT NULL,
-  team VARCHAR(255) NOT NULL,
-  PRIMARY KEY (username, team)
-);
+CREATE INDEX idx_tag_name ON tags (name);
 
 CREATE TABLE actions (
   id VARCHAR(36) NOT NULL,
   from_username VARCHAR(36) NOT NULL,
   to_username VARCHAR(36) NOT NULL,
   status TINYINT NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  CONSTRAINT uq_action UNIQUE (from_username, to_username)
 );
+
+CREATE INDEX idx_action ON actions (from_username, to_username);
 
 CREATE TABLE icons(
   username VARCHAR(36) NOT NULL,
-  icon LONGBLOB NOT NULL
-  mime_type VARCHAR(50) NOT NULL
+  icon LONGBLOB NOT NULL,
+  mime_type VARCHAR(50) NOT NULL,
   PRIMARY KEY (username)
 );
