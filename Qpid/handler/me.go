12a18b6cba2cdd14133c7882e337f35d59ca8841
hiko <bananano343@outlook.jp>
2026-06-27 12:16:25 +0900

    getMeの作成 mock利用

--- Qpid/handler/me.go
+++ Qpid/handler/me.go
@@ -1,10 +1,69 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/traP-jp/hackathon26spring_05/Qpid/domain"
+)
+
+type tagResponse struct {
+	Label    string  `json:"label"`
+	Affinity string  `json:"affinity"`
+	Strength float64 `json:"strength"`
+}
+
+type meResponse struct {
+	Username     string                 `json:"username"`
+	IconFileID   *string                `json:"iconFileId"`
+	Major        *string                `json:"major"`
+	Affiliations []string               `json:"affiliations"`
+	Hometown     *string                `json:"hometown"`
+	Tags         map[string]tagResponse `json:"tags"`
+	Bio          string                 `json:"bio"`
+}
 
 // GET /api/me
 func (h *handler) getMe(c echo.Context) error {
-	return unauthorized(c)
+	if !h.loginUserRetriever.IsUserLoggedIn() {
+		return unauthorized(c)
+	}
+
+	username, err := h.loginUserRetriever.GetLoginUser()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, errorResponse{Message: "failed to get login user"})
+	}
+
+	user, err := h.repository.FindByUsername(username)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, errorResponse{Message: "failed to load user"})
+	}
+	if user == nil {
+		return notFound(c)
+	}
+
+	return c.JSON(http.StatusOK, toMeResponse(*user))
+}
+
+func toMeResponse(user domain.User) meResponse {//FindByUsernameで取得したデータをuserをjsonにして返す用
+	tags := make(map[string]tagResponse, len(user.Tags))
+	for name, tag := range user.Tags {
+		tags[name] = tagResponse{
+			Label:    tag.Label,
+			Affinity: string(tag.Affinity),
+			Strength: tag.Strength,
+		}
+	}
+
+	return meResponse{
+		Username:     user.Username,
+		IconFileID:   user.IconFileID,
+		Major:        user.Major,
+		Affiliations: user.Affiliations,
+		Hometown:     user.Hometown,
+		Tags:         tags,
+		Bio:          user.Bio,
+	}
 }
 
 // PUT /api/me
